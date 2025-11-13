f51bc5596cb024aa2c0e81b838aa27acbd269845
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-11-13 16:15:33 +0000

    Added test 'test00_sync'

--- pts/tests/test00_sync.pct.py
+++ pts/tests/test00_sync.pct.py
@@ -16,7 +16,11 @@ import subprocess
 from pathlib import Path
 import shutil
 import toml
+
 from repoyard import const
+from repoyard.cmds import *
+from repoyard._repos import get_repoyard_meta
+from repoyard.config import get_config
 
 
 # %%
@@ -24,17 +28,86 @@ from repoyard import const
 def test_00_sync(): ...
 
 
+# %% [markdown]
+# Parameters
+
 # %%
 #|export
-tests_working_dir = const.pkg_path.parent / "tmp_tests"
-test_folder_path = tests_working_dir / "test_00_sync"
-if test_folder_path.exists():
-    shutil.rmtree(test_folder_path)
-test_folder_path.mkdir(parents=True, exist_ok=True)
+num_test_repos = 20
+
+# %% [markdown]
+# # Initialise `init_repoyard`
 
 # %%
-home_path = test_folder_path / "home"
-config_path = home_path / ".config" / "repoyard" / "config.toml"
-data_path = home_path / ".repoyard"
+#|export
+# Set up test folders
+import tempfile
+test_folder_path = Path(tempfile.mkdtemp(prefix="sync_repo", dir="/tmp"))
+test_folder_path.mkdir(parents=True, exist_ok=True)
+config_path = test_folder_path / ".config" / "repoyard" / "config.toml"
+data_path = test_folder_path / ".repoyard"
 
-storage_loc_path = test_folder_path / "storage_location"
+# Run init
+init_repoyard(config_path=config_path, data_path=data_path)
+
+# Add a storage location 'my_remote'
+import toml
+test_storage_location_name = "my_remote"
+config_dump = toml.load(config_path)
+remote_rclone_path = Path(tempfile.mkdtemp(prefix="rclone_remote", dir="/tmp"))
+config_dump['storage_locations'][test_storage_location_name] = {
+    'storage_type' : "rclone",
+    'store_path' : "repoyard",
+}
+config_path.write_text(toml.dumps(config_dump))
+
+# Load config
+config = get_config(config_path)
+
+# Set up a rclone remote path for testing
+config.rclone_config_path.write_text(f"""
+[{test_storage_location_name}]
+type = alias
+remote = {remote_rclone_path}
+""");
+
+# %% [markdown]
+# # Create some repos using `new_repo` and sync them using `sync_repo`
+
+# %%
+#|export
+repo_full_names = []
+for i in range(num_test_repos):
+    repo_full_name = new_repo(config_path=config_path, repo_name=f"test_repo_{i}", storage_location=test_storage_location_name)
+    repo_full_names.append(repo_full_name)
+    
+# Verify that the repos are included
+repoyard_meta = get_repoyard_meta(config, force_create=True)
+for repo_full_name in repo_full_names:
+    assert repoyard_meta.by_full_name[repo_full_name].check_included(config)
+
+# %% [markdown]
+# # Exclude all repos using `exclude_repo`
+
+# %%
+#|export
+for repo_full_name in repo_full_names:
+    exclude_repo(config_path=config_path, repo_full_name=repo_full_name)
+    
+# Verify that the repos have been excluded
+repoyard_meta = get_repoyard_meta(config, force_create=True)
+for repo_full_name in repo_full_names:
+    assert not repoyard_meta.by_full_name[repo_full_name].check_included(config)
+
+# %% [markdown]
+# # Include all repos using `include_repo`
+
+# %%
+#|export
+for repo_full_name in repo_full_names:
+    include_repo(config_path=config_path, repo_full_name=repo_full_name)
+    
+# Verify that the repos are included
+repoyard_meta = get_repoyard_meta(config, force_create=True)
+for repo_full_name in repo_full_names:
+    assert repoyard_meta.by_full_name[repo_full_name].check_included(config)
